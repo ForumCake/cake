4f24add0cd453a1a69596cc3fe03967e1b2ab706
Jonathan Wainwright <jon@waindigo.org>
2015-07-10 13:07:34 +0100

    Fixes "Undefined index: module_name_cake" exception on uninstall

--- library/Cake/Admin.php
+++ library/Cake/Admin.php
@@ -9,7 +9,7 @@ class Admin
         foreach ($params['adminNavigation']['sideLinks'] as $section => $sideLinks) {
             $sideLinkCount = count($sideLinks);
             foreach ($sideLinks as $adminNavigationId => $adminNavigation) {
-                if ($adminNavigation['module_name_cake']) {
+                if (!empty($adminNavigation['module_name_cake'])) {
                     $modules = \Cake\Proxy::getOptionValue('modules', $adminNavigation['addon_id']);
                     if (!isset($modules[$adminNavigation['module_name_cake']])) {
                         unset($params['adminNavigation']['sideLinks'][$section][$adminNavigationId]);
